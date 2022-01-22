51db5576f5a51747cf1da45ba7148eda732553eb
hkr0416 <hkr0416@gmail.com>
2022-01-22 17:09:44 +0900

    main, search, item component

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,9 +1,25 @@
+import dummyItems from '../dummy/dummyItems';
 import Nav from '../components/Nav';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import Search from '../components/Search';
+import Item from '../components/Item';
 
 function Main () {
+  const [searchedItem, setSearchedItem] = useState([]);
+
   return (
     <div>
-      <h1>This is Main</h1>
+      <Search setSearchedItem={setSearchedItem} />
+      <button>리뷰작성</button>
+      <section>
+        {/* 나중에 dummydata 대신에 searchedItem으로 렌더링 */}
+        {dummyItems.map((item, idx) => {
+          return (          
+            <Link to='/iteminfo' key={idx}><Item item={item} /></Link>
+          )
+        })}
+      </section>      
 
     </div>
   );
