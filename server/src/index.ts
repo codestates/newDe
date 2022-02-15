76871c5eea3b34027ca3282045be16d0073f96be
hkr0416 <hkr0416@gmail.com>
2022-02-15 15:59:13 +0900

    typeorm setting

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+//import compression from 'compression';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan('dev');
+
 
 app.use(logger);
 app.use(cors({
