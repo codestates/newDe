a38a40133a0f2c03d420b706c4e2481cd4af4f88
hkr0416 <hkr0416@gmail.com>
2022-02-16 00:07:17 +0900

    for push

---
+++ server/src/entity/content.ts
@@ -0,0 +1,34 @@
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    main: string;
+
+    @Column()
+    userId: number;
+
+    @Column()
+    like: number;
+
+    @Column()
+    report: number;
+
+    @Column()
+    parentCategory: string;
+
+    @Column()
+    childCategory: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
