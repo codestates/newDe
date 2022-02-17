3f7d62b58f192fdab66cd52d1ce360edbc903f0d
shjthd92 <shjthd92@naver.com>
2022-02-17 15:36:46 +0900

    게시판 하드코딩

--- client/src/pages/MainBoard.tsx
+++ client/src/pages/MainBoard.tsx
@@ -24,8 +24,8 @@ const BoardWrap = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
-width: 60%;
-background: brown;
+width: 80%;
+
 @media ${(props)=> props.theme.mobile}{
     width: 100%;
     height: 100%;
@@ -39,49 +39,125 @@ const BoardName = styled.div`
 
 width : 80%;
 text-align: left;
-background: yellow;
+
 font-size: 1.5rem;
 margin: 2%;
+margin-top: 5%;
 `
 
 const ChildBoard = styled.div`
 display: flex;
 flex-direction: column;
 width: 90%;
-background: green;
+
 margin-top: 1%;
 align-items: center;
 text-align: center;
+margin-bottom: 5%;
 
 `
 
 const ContentWrap = styled.div`
+display: flex;
+justify-content: center;
 width: 90%;
-background: white;
-margin: 0.5% 0.5% 0.5% 0.5% 
+align-items: center;
+margin: 0.5% 0.5% 0.5% 0.5%
 
 
 `
 
+const Contenttitle = styled.div`
+width: 70%;
+
+text-align: left;`
+
+
+const Contentusersec = styled.div`
+width: 10%;
+`
+
+const Contentlike = styled.div`
+width: 10%;
+`
 
 function MainBoard():JSX.Element{
+    //axios를 이용 게시글 정보 받아와서 렌더링  
+    //http://localhost:8080/board/front, /back 을 각각 가져와서 최근 5개씩 렌더링 
+    
 
     
     return(
         <MainContainer>
     
 
-        <BoardWrap>게시판 영역입니다. 
+        <BoardWrap>
             <BoardName>프론트 게시판</BoardName>
             <ChildBoard>
-                <ContentWrap>글 1</ContentWrap>
-                <ContentWrap>글 2</ContentWrap>
+                <ContentWrap>
+                    <Contenttitle>제목</Contenttitle>
+                    <Contentusersec>글쓴이</Contentusersec>
+                    <Contentlike>추천</Contentlike>
+
+                </ContentWrap>
+                <ContentWrap>
+                    <Contenttitle>제목</Contenttitle>
+                    <Contentusersec>글쓴이</Contentusersec>
+                    <Contentlike>추천</Contentlike>
+
+                </ContentWrap>
+                <ContentWrap>
+                    <Contenttitle>제목</Contenttitle>
+                    <Contentusersec>글쓴이</Contentusersec>
+                    <Contentlike>추천</Contentlike>
+
+                </ContentWrap>
+                <ContentWrap>
+                    <Contenttitle>제목</Contenttitle>
+                    <Contentusersec>글쓴이</Contentusersec>
+                    <Contentlike>추천</Contentlike>
+
+                </ContentWrap>
+                <ContentWrap>
+                    <Contenttitle>제목</Contenttitle>
+                    <Contentusersec>글쓴이</Contentusersec>
+                    <Contentlike>추천</Contentlike>
+
+                </ContentWrap>
             </ChildBoard>
 
             <BoardName>백엔드 게시판</BoardName>
             <ChildBoard>
-                <ContentWrap>글 1</ContentWrap>
-                <ContentWrap>글 2</ContentWrap>
+            <ContentWrap>
+                    <Contenttitle>제목</Contenttitle>
+                    <Contentusersec>글쓴이</Contentusersec>
+                    <Contentlike>추천</Contentlike>
+
+                </ContentWrap>
+                <ContentWrap>
+                    <Contenttitle>제목</Contenttitle>
+                    <Contentusersec>글쓴이</Contentusersec>
+                    <Contentlike>추천</Contentlike>
+
+                </ContentWrap>
+                <ContentWrap>
+                    <Contenttitle>제목</Contenttitle>
+                    <Contentusersec>글쓴이</Contentusersec>
+                    <Contentlike>추천</Contentlike>
+
+                </ContentWrap>
+                <ContentWrap>
+                    <Contenttitle>제목</Contenttitle>
+                    <Contentusersec>글쓴이</Contentusersec>
+                    <Contentlike>추천</Contentlike>
+
+                </ContentWrap>
+                <ContentWrap>
+                    <Contenttitle>제목</Contenttitle>
+                    <Contentusersec>글쓴이</Contentusersec>
+                    <Contentlike>추천</Contentlike>
+
+                </ContentWrap>
             </ChildBoard>
 
 
