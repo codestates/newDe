916a31687a5fab012f60c4f76c2aca6afe430f1e
hkr0416 <hkr0416@gmail.com>
2022-02-22 21:12:37 +0900

    merge feature, tsc

--- server/dist/controllers/userController.js
+++ server/dist/controllers/userController.js
@@ -133,7 +133,6 @@ const checkInfo = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
             return res.status(403).json({ message: 'Invalid Accesstoken' });
         const userRepository = (0, typeorm_1.getRepository)(user_1.User);
         const userInfo = yield userRepository.findOne({ email: verify.userInfo.email });
-        console.log(userInfo);
         if (userInfo.password === password) {
             return res.status(200).json({ message: 'password correct!' });
         }
