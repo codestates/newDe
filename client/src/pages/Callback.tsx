cd67dbf170701cfcb47c287ef3b60085575af95e
shjthd92 <shjthd92@naver.com>
2022-02-25 12:15:50 +0900

    kakaologin modified for redux

--- client/src/pages/Callback.tsx
+++ client/src/pages/Callback.tsx
@@ -1,25 +1,33 @@
 import Loader from '../component/Loader'
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { apiURL } from '../url'
 import axios from 'axios'
+import { RootState } from '../store'
+import { useAppSelector, useAppDispatch } from '../store/hooks'
 import { useState } from 'react';
+import { setLogin } from '../features/info';
 
 function Callback(props:any):JSX.Element {
 
     const [loading, setLoading] = useState(false);
 
     const navigate = useNavigate()
+    const dispatch = useAppDispatch()
 
     useEffect( () => {
-        console.log(props)
+        // console.log(props)
         setLoading(true)
         const url = new URL(window.location.href);
 
         const login = url.searchParams.get('islogin')
         if(login === 'success') {
-            console.log(props.loginhandler)
-            props.loginhandler()
+            // console.log(setLogin(true))
+            // console.log(props.loginhandler)
+            // props.loginhandler()
+            dispatch(setLogin(true))
+            
             navigate('/MyPage')
             }
         
