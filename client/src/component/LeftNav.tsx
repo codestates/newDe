5e6097e3f0130cd3c73415c2deb7db5175e9867b
hkr0416 <hkr0416@gmail.com>
2022-03-03 16:55:34 +0900

    delete back javascript

--- client/src/component/LeftNav.tsx
+++ client/src/component/LeftNav.tsx
@@ -16,7 +16,7 @@ const ContainerWrap = styled.div`
     color: black;
     }
 &.target {
-    color: blue;
+    color: black;
     text-shadow: 2px 2px 2px gray;
 }
 }`
@@ -56,19 +56,18 @@ function Leftnav (props:any):JSX.Element  {
                 <FrontCategory><Link to = '/board?parentcategory=front' className = {`btn ${targetBoard[0]==='front' && !targetBoard[1]?'target':''}`}>Front</Link></FrontCategory>
                 <ChildMenuWrap><Link to = '/board?parentcategory=front&childcategory=html' className = {`btn ${targetBoard[1]==='html'?'target':''}`}>HTML</Link></ChildMenuWrap>
                 <ChildMenuWrap><Link to = '/board?parentcategory=front&childcategory=css' className = {`btn ${targetBoard[1]==='css'?'target':''}`}>CSS</Link></ChildMenuWrap>
-                <ChildMenuWrap><Link to = '/board?parentcategory=front&childcategory=javascript' className = {`btn ${targetBoard[1]==='javascript' && targetBoard[0] === 'front'?'target':''}`}>JavaScript</Link></ChildMenuWrap>
+                <ChildMenuWrap><Link to = '/board?parentcategory=front&childcategory=javascript' className = {`btn ${targetBoard[1]==='javascript'?'target':''}`}>JavaScript</Link></ChildMenuWrap>
                 <ChildMenuWrap><Link to = '/board?parentcategory=front&childcategory=react' className = {`btn ${targetBoard[1]==='react'?'target':''}`}>React</Link></ChildMenuWrap>
-                <ChildMenuWrap><Link to = '/board?parentcategory=front&childcategory=프론트 기타' className = {`btn ${targetBoard[1]==='프론트 기타'?'target':''}`}>기타</Link></ChildMenuWrap>
+                <ChildMenuWrap><Link to = '/board?parentcategory=front&childcategory=프론트기타' className = {`btn ${targetBoard[1]==='프론트기타'?'target':''}`}>기타</Link></ChildMenuWrap>
             </ParentMenuWrap>
             <ParentMenuWrap>
                 <BackCategory><Link to = '/board?parentcategory=back' className = {`btn ${targetBoard[0]==='back' && !targetBoard[1]?'target':''}`}>Back</Link></BackCategory>
                 <ChildMenuWrap><Link to = '/board?parentcategory=back&childcategory=php' className = {`btn ${targetBoard[1]==='php'?'target':''}`}>PHP</Link></ChildMenuWrap>
                 <ChildMenuWrap><Link to = '/board?parentcategory=back&childcategory=node.js' className = {`btn ${targetBoard[1]==='node.js'?'target':''}`}>Node.js</Link></ChildMenuWrap>
-                <ChildMenuWrap><Link to = '/board?parentcategory=back&childcategory=javascript' className = {`btn ${targetBoard[1]==='javascript' && targetBoard[0]==='back'?'target':''}`}>JavaScript</Link></ChildMenuWrap>
                 <ChildMenuWrap><Link to = '/board?parentcategory=back&childcategory=java' className = {`btn ${targetBoard[1]==='java'?'target':''}`}>Java</Link></ChildMenuWrap>
                 <ChildMenuWrap><Link to = '/board?parentcategory=back&childcategory=python' className = {`btn ${targetBoard[1]==='python'?'target':''}`}>Python</Link></ChildMenuWrap>
                 <ChildMenuWrap><Link to = '/board?parentcategory=back&childcategory=server' className = {`btn ${targetBoard[1]==='server'?'target':''}`}>서버</Link></ChildMenuWrap>
-                <ChildMenuWrap><Link to = '/board?parentcategory=back&childcategory=백엔드 기타' className = {`btn ${targetBoard[1]==='백엔드 기타'?'target':''}`}>기타</Link></ChildMenuWrap>
+                <ChildMenuWrap><Link to = '/board?parentcategory=back&childcategory=백엔드기타' className = {`btn ${targetBoard[1]==='백엔드기타'?'target':''}`}>기타</Link></ChildMenuWrap>
             </ParentMenuWrap>
         </ContainerWrap>       
     )
